9c1cee7726dec266a9a98d6ff0ab2cb7264a5017
Gentry Atkinson <gma23@txstate.edu>
2020-12-10 19:34:10 -0600

    added a url to the list

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
-let recognizer;
+let model, mic, result;
 
-function predictWord() {
+function predictBark(scores) {
  // Array of words that the recognizer is trained to recognize.
- const words = recognizer.wordLabels();
- recognizer.listen(({scores}) => {
+ const words = ['bark', 'no bark'];
+ console.log("Got scores, ", scores);
+ //recognizer.listen(({scores}) => {
    // Turn scores into a list of (score,word) pairs.
-   scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
+   //scores = Array.from(scores).map((s, i) => ({score: s, word: words[i]}));
    // Find the most probable word.
-   scores.sort((s1, s2) => s2.score - s1.score);
-   document.querySelector('#console').textContent = scores[0].word;
- }, {probabilityThreshold: 0.75});
+   //scores.sort((s1, s2) => s2.score - s1.score);
+   //document.querySelector('#console').textContent = scores[0].word;
+ //}, {probabilityThreshold: 0.75});
+
+
 }
 
 async function app() {
- recognizer = speechCommands.create('BROWSER_FFT');
- await recognizer.ensureModelLoaded();
- predictWord();
+  const modelUrl = 'https://tfhub.dev/google/tfjs-model/yamnet/tfjs/1';
+  const model = await tf.loadGraphModel(modelUrl, { fromTFHub: true });
+
+
+  const mic = await tf.data.microphone({
+   fftSize: 1024,
+   columnTruncateLength: 232,
+   numFramesPerSpectrogram: 43,
+   sampleRateHz:44100,
+   includeSpectrogram: true,
+   includeWaveform: true
+  });
+  const audioData = await mic.capture();
+  const spectrogramTensor = audioData.spectrogram;
+  spectrogramTensor.print();
+  const waveformTensor = audioData.waveform;
+  waveformTensor.print();
+  mic.stop();
+  const waveform = tf.zeros([16000 * 3]);
+  const [scores, embeddings, spectrogram] = model.predict(waveform);
+  scores.print(verbose=true);  // shape [N, 521]
+  embeddings.print(verbose=true);  // shape [N, 1024]
+  spectrogram.print(verbose=true);  // shape [M, 64]
+  //Find class with the top score when mean-aggregated across frames.
+  scores.mean(axis=0).argMax().print(verbose=true);
+  //Should print 494 corresponding to 'Silence' in YAMNet Class Map.
+  predictBark(scores);
 }
+
 
 app();
